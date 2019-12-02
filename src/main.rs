6a41a707fa77d3f48a4d4ba55a86e0e34066d0e3
Charles Pierce <cpierce.grad@gmail.com>
2019-12-01 22:02:36 -0800

    Day 1 - Part 2

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,34 @@ fn main() {
     let reader = BufReader::new(file);
 
     for mass in reader.lines() {
-        total_fuel += fuel_for_mass(mass.unwrap().parse().unwrap());
+        total_fuel += fuel_for_module(mass.unwrap().parse().unwrap());
     }
 
     println!("Fuel: {}", total_fuel);
 }
 
-fn fuel_for_mass(mass: u32) -> u32 {
-    (mass / 3) -2
+fn fuel_for_module(mass: i32) -> i32 {
+    let mut fuel_needed = 0;
+    let mut current_fuel = fuel_for_mass(mass);
+
+    loop {
+        fuel_needed += current_fuel;
+        current_fuel = fuel_for_mass(current_fuel);
+
+        if current_fuel == 0 {
+            break;
+        }
+    }
+
+    fuel_needed
+}
+
+fn fuel_for_mass(mass: i32) -> i32 {
+    let fuel = (mass / 3) - 2;
+
+    if fuel < 0 {
+        0
+    } else {
+        fuel
+    }
 }
