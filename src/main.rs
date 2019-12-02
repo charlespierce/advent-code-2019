9624e1fc43f733fe6e65d04649255d9872390c39
Charles Pierce <cpierce.grad@gmail.com>
2019-12-01 21:55:26 -0800

    Day 1 - Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let mut total_fuel = 0;
+    let file = File::open("input.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    for mass in reader.lines() {
+        total_fuel += fuel_for_mass(mass.unwrap().parse().unwrap());
+    }
+
+    println!("Fuel: {}", total_fuel);
+}
+
+fn fuel_for_mass(mass: u32) -> u32 {
+    (mass / 3) -2
 }
