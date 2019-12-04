2d650aa9c974c0e400b325d3b5d78761297fb344
Charles Pierce <cpierce.grad@gmail.com>
2019-12-03 23:30:44 -0800

    Day 4 - Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    let mut valid_count = 0;
+    for i in 172_851..675_870 {
+        if is_valid(i) {
+            valid_count += 1;
+        }
+    }
+
+    println!("Valid Passwords: {}", valid_count);
+}
+
+fn is_valid(input: u32) -> bool {
+    let input_str = input.to_string();
+    let digits = input_str.chars();
+    let mut has_double = false;
+    let mut prev_char: Option<char> = None;
+
+    for chr in digits {
+        if let Some(prev) = prev_char {
+            if chr < prev {
+                return false;
+            }
+            if chr == prev {
+                has_double = true;
+            }
+        }
+
+        prev_char = Some(chr);
+    }
+
+    has_double
 }
