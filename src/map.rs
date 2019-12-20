a55fdac973ba5f243481544d8b07c5b58acff2cd
Charles Pierce <cpierce.grad@gmail.com>
2019-12-19 23:36:09 -0800

    Day 20 - Part 2

--- src/map.rs
+++ src/map.rs
@@ -1,12 +1,13 @@
 use std::collections::hash_map::Entry;
 use std::collections::{HashMap, HashSet};
 
-use crate::point::Point;
+use crate::point::{Location, Point};
 
 #[derive(Debug)]
 pub struct Map {
-    pub tunnels: HashSet<Point>,
-    pub portals: HashMap<Point, Point>,
+    pub tunnels: HashSet<Location>,
+    pub inner_portals: HashMap<Location, Location>,
+    pub outer_portals: HashMap<Location, Location>,
     pub start: Point,
     pub end: Point,
 }
@@ -21,13 +22,19 @@ impl Map {
             from.move_west(1),
             from.move_south(1),
         ] {
-            if self.tunnels.contains(point) {
+            if self.tunnels.contains(&point.loc) {
                 output.push(*point);
             }
         }
 
-        if let Some(other) = self.portals.get(&from) {
-            output.push(*other);
+        if from.level > 0 {
+            if let Some(other) = self.outer_portals.get(&from.loc) {
+                output.push(Point::new(*other, from.level - 1));
+            }
+        }
+
+        if let Some(other) = self.inner_portals.get(&from.loc) {
+            output.push(Point::new(*other, from.level + 1));
         }
 
         output
@@ -36,35 +43,54 @@ impl Map {
 
 impl From<String> for Map {
     fn from(value: String) -> Self {
-        let map: HashMap<Point, char> = value
+        let map: HashMap<Location, char> = value
             .lines()
             .enumerate()
             .flat_map(|(y, line)| {
                 line.chars()
                     .enumerate()
-                    .map(move |(x, chr)| (Point::new(x as i64, y as i64), chr))
+                    .map(move |(x, chr)| (Location::new(x as i64, y as i64), chr))
             })
             .collect();
 
-        let mut tunnels = HashSet::new();
-        let mut portals = HashMap::new();
-        let mut portal_temp: HashMap<String, Point> = HashMap::new();
+        let (max_x, max_y) = map.keys().fold((0, 0), |(mx, my), Location { x, y }| {
+            let x_val = mx.max(*x);
+            let y_val = my.max(*y);
+            (x_val, y_val)
+        });
 
-        for (point, chr) in map.iter() {
+        let mut tunnels = HashSet::new();
+        let mut inner_portals = HashMap::new();
+        let mut outer_portals = HashMap::new();
+        let mut portal_temp: HashMap<String, Location> = HashMap::new();
+
+        for (loc, chr) in map.iter() {
             match chr {
                 '.' => {
-                    tunnels.insert(*point);
+                    tunnels.insert(*loc);
                 }
                 label if 'A' <= *label && *label <= 'Z' => {
-                    if let Some((label, portal)) = find_label_tunnel(point, &map) {
+                    if let Some((label, portal)) = find_label_tunnel(loc, &map) {
                         match portal_temp.entry(label) {
                             Entry::Vacant(e) => {
                                 e.insert(portal);
                             }
                             Entry::Occupied(e) => {
                                 let other = e.remove();
-                                portals.insert(portal, other);
-                                portals.insert(other, portal);
+
+                                if portal.x == 2
+                                    || portal.x == max_x - 2
+                                    || portal.y == 2
+                                    || portal.y == max_y - 2
+                                {
+                                    // Outer
+                                    outer_portals.insert(portal, other);
+                                    inner_portals.insert(other, portal);
+                                } else {
+                                    // Inner
+                                    inner_portals.insert(portal, other);
+                                    outer_portals.insert(other, portal);
+                                }
                             }
                         }
                     }
@@ -75,14 +101,18 @@ impl From<String> for Map {
 
         Map {
             tunnels,
-            portals,
-            start: portal_temp.remove("AA").unwrap(),
-            end: portal_temp.remove("ZZ").unwrap(),
+            inner_portals,
+            outer_portals,
+            start: Point::new(portal_temp.remove("AA").unwrap(), 0),
+            end: Point::new(portal_temp.remove("ZZ").unwrap(), 0),
         }
     }
 }
 
-fn find_label_tunnel(label_start: &Point, map: &HashMap<Point, char>) -> Option<(String, Point)> {
+fn find_label_tunnel(
+    label_start: &Location,
+    map: &HashMap<Location, char>,
+) -> Option<(String, Location)> {
     let mut label = String::new();
     label.push(*map.get(&label_start).unwrap());
 
--- src/point.rs
+++ src/point.rs
@@ -2,35 +2,69 @@ use std::fmt;
 use std::hash::Hash;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Location {
+    pub x: i64,
+    pub y: i64,
+}
+
+impl Location {
+    pub fn new(x: i64, y: i64) -> Self {
+        Self { x, y }
+    }
+
+    pub fn move_north(&self, distance: i64) -> Self {
+        Self::new(self.x, self.y - distance)
+    }
+
+    pub fn move_south(&self, distance: i64) -> Self {
+        Self::new(self.x, self.y + distance)
+    }
+
+    pub fn move_west(&self, distance: i64) -> Self {
+        Self::new(self.x - distance, self.y)
+    }
+
+    pub fn move_east(&self, distance: i64) -> Self {
+        Self::new(self.x + distance, self.y)
+    }
+}
+
+impl fmt::Display for Location {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Point {
-    x: i64,
-    y: i64,
+    pub loc: Location,
+    pub level: i64,
 }
 
 impl Point {
-    pub fn new(x: i64, y: i64) -> Point {
-        Point { x, y }
+    pub fn new(loc: Location, level: i64) -> Point {
+        Point { loc, level }
     }
 
     pub fn move_north(&self, distance: i64) -> Point {
-        Point::new(self.x, self.y - distance)
+        Point::new(self.loc.move_north(distance), self.level)
     }
 
     pub fn move_south(&self, distance: i64) -> Point {
-        Point::new(self.x, self.y + distance)
+        Point::new(self.loc.move_south(distance), self.level)
     }
 
     pub fn move_west(&self, distance: i64) -> Point {
-        Point::new(self.x - distance, self.y)
+        Point::new(self.loc.move_west(distance), self.level)
     }
 
     pub fn move_east(&self, distance: i64) -> Point {
-        Point::new(self.x + distance, self.y)
+        Point::new(self.loc.move_east(distance), self.level)
     }
 }
 
 impl fmt::Display for Point {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "({}, {})", self.x, self.y)
+        write!(f, "{} - {}", self.loc, self.level)
     }
 }
