49a3b813160830d616efcfa4d1f104805a03f38f
Charles Pierce <cpierce.grad@gmail.com>
2019-12-22 14:21:31 -0800

    Day 22 - Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,94 @@
 use std::fs::read_to_string;
 
-fn main() {
-    let mut deck = Vec::with_capacity(10_007);
-    for i in 0..10_007 {
-        deck.push(i as u64);
-    }
+use mod_exp::mod_exp;
 
-    let shuffled = read_to_string("input.txt")
+fn main() {
+    let single = read_to_string("input.txt")
         .unwrap()
         .lines()
         .map(Move::from)
-        .fold(deck, |d, m| m.apply(d));
+        .fold(Deck::new(119_315_717_514_047), |deck, mv| {
+            deck.apply_move(mv)
+        });
 
-    let card_index = shuffled
-        .iter()
-        .enumerate()
-        .find_map(|(i, val)| if *val == 2019 { Some(i) } else { None })
-        .unwrap();
+    let shuffled = single.iterate(101_741_582_076_661);
 
-    println!("Card 2019 is at: {}", card_index);
+    println!("Position 2020: {}", shuffled.value(2020));
+}
+
+struct Deck {
+    offset: i128,
+    increment: i128,
+    size: i128,
+}
+
+impl Deck {
+    fn new(size: i128) -> Self {
+        Deck {
+            offset: 0,
+            increment: 1,
+            size,
+        }
+    }
+
+    fn iterate(self, i: i128) -> Self {
+        let increment = mod_exp(self.increment, i, self.size);
+        let inv = mod_exp(
+            i128::rem_euclid(1 - self.increment, self.size),
+            self.size - 2,
+            self.size,
+        );
+        let product = i128::rem_euclid(1 - mod_exp(self.increment, i, self.size), self.size);
+        let product = i128::rem_euclid(product * inv, self.size);
+        let offset = i128::rem_euclid(self.offset * product, self.size);
+
+        Deck {
+            offset,
+            increment,
+            size: self.size,
+        }
+    }
+
+    fn value(&self, index: i128) -> i128 {
+        let step = i128::rem_euclid(self.increment * index, self.size);
+        i128::rem_euclid(self.offset + step, self.size)
+    }
+
+    fn apply_move(self, mv: Move) -> Self {
+        match mv {
+            Move::NewStack => {
+                let increment = i128::rem_euclid(-self.increment, self.size);
+                Deck {
+                    offset: i128::rem_euclid(self.offset + increment, self.size),
+                    increment,
+                    size: self.size,
+                }
+            }
+            Move::Cut(loc) => {
+                let actual_cut = i128::rem_euclid(loc, self.size);
+                let step = i128::rem_euclid(actual_cut * self.increment, self.size);
+                Deck {
+                    offset: i128::rem_euclid(self.offset + step, self.size),
+                    increment: self.increment,
+                    size: self.size,
+                }
+            }
+            Move::Increment(step) => {
+                let inv = mod_exp(step, self.size - 2, self.size);
+                Deck {
+                    offset: self.offset,
+                    increment: i128::rem_euclid(self.increment * inv, self.size),
+                    size: self.size,
+                }
+            }
+        }
+    }
 }
 
 enum Move {
     NewStack,
-    Cut(i64),
-    Increment(i64),
-}
-
-impl Move {
-    fn apply(self, mut deck: Vec<u64>) -> Vec<u64> {
-        match self {
-            Move::NewStack => deck.into_iter().rev().collect(),
-            Move::Cut(len) => {
-                let index = if len < 0 {
-                    let size = deck.len() as i64;
-                    (size + len) as usize
-                } else {
-                    len as usize
-                };
-                let mut bottom = deck.split_off(index);
-                bottom.append(&mut deck);
-                bottom
-            }
-            Move::Increment(step) => {
-                let mut result = Vec::from(&*deck);
-                let mut index = 0;
-
-                for val in deck {
-                    result[index] = val;
-                    index += step as usize;
-                    index %= result.len();
-                }
-
-                result
-            }
-        }
-    }
+    Cut(i128),
+    Increment(i128),
 }
 
 impl From<&str> for Move {
