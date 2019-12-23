9fa6c70a53991a3d85c1d3b2b731120ac5e6bffc
Charles Pierce <cpierce.grad@gmail.com>
2019-12-22 21:47:00 -0800

    Day 23 - Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,132 @@
-use std::convert::TryFrom;
-use std::str::Chars;
+use std::collections::VecDeque;
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::thread;
 
 mod computer;
 
 use computer::{Computer, IO};
 
 fn main() {
-    let computer = Computer::new();
-    let mut ascii = Ascii::new();
+    let mut network = Network::new();
 
-    computer.run(&mut ascii);
+    for _ in 0..50 {
+        network.add_element();
+    }
+
+    network.watch_messages();
 }
 
-const INPUT: &str = "NOT A J
-NOT B T
-OR T J
-NOT C T
-OR J T
-AND D T
-NOT E J
-NOT J J
-OR H J
-AND T J
-RUN
-";
-
-struct Ascii {
-    chars: Chars<'static>,
+struct Packet {
+    x: i64,
+    y: i64,
 }
 
-impl Ascii {
+struct Network {
+    elements: Vec<NetworkElement>,
+}
+
+impl Network {
     fn new() -> Self {
-        Ascii {
-            chars: INPUT.chars(),
+        Network {
+            elements: Vec::new(),
+        }
+    }
+
+    fn add_element(&mut self) {
+        let index = self.elements.len() as i64;
+        let (message_sender, message_receiver) = channel();
+        let (packet_sender, packet_receiver) = channel();
+        self.elements.push(NetworkElement {
+            sender: packet_sender,
+            receiver: message_receiver,
+        });
+
+        thread::spawn(move || {
+            let computer = Computer::new();
+            let mut io = NicIO::new(index, message_sender, packet_receiver);
+
+            computer.run(&mut io);
+        });
+    }
+
+    fn watch_messages(&mut self) {
+        loop {
+            for element in self.elements.iter() {
+                if let Ok(message) = element.receiver.try_recv() {
+                    match message.destination {
+                        255 => {
+                            println!("Message to 255: {}", message.packet.y);
+                            panic!("Done!");
+                        }
+                        a if 0 <= a && a < 50 => {
+                            self.elements[a as usize]
+                                .sender
+                                .send(message.packet)
+                                .unwrap();
+                        }
+                        _ => {}
+                    }
+                }
+            }
         }
     }
 }
 
-impl IO for Ascii {
-    fn next_input(&mut self) -> i64 {
-        self.chars.next().unwrap() as i64
+struct NetworkElement {
+    sender: Sender<Packet>,
+    receiver: Receiver<Message>,
+}
+
+struct Message {
+    destination: i64,
+    packet: Packet,
+}
+
+struct NicIO {
+    queue: VecDeque<i64>,
+    network_sender: Sender<Message>,
+    network_receiver: Receiver<Packet>,
+    partial_message: VecDeque<i64>,
+}
+
+impl NicIO {
+    fn new(address: i64, sender: Sender<Message>, receiver: Receiver<Packet>) -> Self {
+        NicIO {
+            queue: VecDeque::from(vec![address]),
+            network_sender: sender,
+            network_receiver: receiver,
+            partial_message: VecDeque::new(),
+        }
     }
 
-    fn next_output(&mut self, value: i64) {
-        match char::try_from(value as u32) {
-            Ok(chr) => print!("{}", chr),
-            Err(_) => println!("{}", value),
+    fn check_messages(&mut self) {
+        if let Ok(packet) = self.network_receiver.try_recv() {
+            self.queue.push_back(packet.x);
+            self.queue.push_back(packet.y);
+        }
+    }
+}
+
+impl IO for NicIO {
+    fn next_input(&mut self) -> i64 {
+        self.check_messages();
+        match self.queue.pop_front() {
+            Some(val) => val,
+            None => -1,
+        }
+    }
+
+    fn next_output(&mut self, val: i64) {
+        self.partial_message.push_back(val);
+        if self.partial_message.len() == 3 {
+            let message = Message {
+                destination: self.partial_message.pop_front().unwrap(),
+                packet: Packet {
+                    x: self.partial_message.pop_front().unwrap(),
+                    y: self.partial_message.pop_front().unwrap(),
+                },
+            };
+            self.network_sender.send(message).unwrap();
         }
     }
 }
